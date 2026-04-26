6fea48b101c9b379fcdafd8caf08eed00a8ffaf7
ghita <ghitazouhri09@gmail.com>
2026-04-26 21:18:48 +0200

    troisième  commit : projet Django

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .views import HomeView
+from django.contrib.auth import views as auth_views
+
 urlpatterns =[
     path('', HomeView.as_view(),name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name="stages-emsi/login.html"), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
