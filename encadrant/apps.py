b62248ed7b750777b3d5b8bc4657859436af310b
ghita <ghitazouhri09@gmail.com>
2026-04-26 17:06:03 +0200

    premier commit : projet Django

---
+++ encadrant/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EncadrantConfig(AppConfig):
+    name = 'encadrant'
---
+++ étudiant/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ÉtudiantConfig(AppConfig):
+    name = 'étudiant'
